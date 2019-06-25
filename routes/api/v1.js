23f4bf10d58b4b8f8c6767179a41dd933aff6981
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-06-25 22:36:17 +0900

    api structure

--- routes/api/v1.js
+++ routes/api/v1.js
@@ -1,9 +1,40 @@
+/*jshint esversion: 9 */
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.end();
 });
 
+router.get('/search/:movie', (req, res, next) => {
+  res.end();
+});
+
+router.get('/search/watcha/:movie', (req, res, next) => {
+
+});
+
+router.get('/search/imdb/:movie', (req, res, next) => {
+
+});
+
+router.get('/movie/:id', (req, res, next) => {
+
+});
+
+router.get('/movie/watcha/:id', (req, res, next) => {
+  if (true) {
+
+  }
+});
+
+router.get('/movie/imdb/:id', (req, res, next) => {
+
+});
+
+router.get('/id/:movie', (req, res, next) => {
+
+});
 module.exports = router;
