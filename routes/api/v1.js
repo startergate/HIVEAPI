05db55d1de27ba6f4c297e9e2880d2a7eb2fc6ad
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-06-27 20:55:00 +0900

    kia~ 받아와진다!

--- routes/api/v1.js
+++ routes/api/v1.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const request = require('request');
+const axios = require('axios');
 const cheerio = require('cheerio');
 const iconv = require('iconv-lite');
 const getHtml = require('../../models/axiosRequest');
@@ -13,43 +14,49 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/search/:movie', (req, res, next) => {
-  const options = {
-    uri: "https://watcha.com/ko-KR/search?query=" + req.params.movie,
-    encoding: "utf8",
+  const instance = axios.create({
+    baseURL: 'https://watcha.com/ko-KR/',
+    timeout: 1000,
     headers: {
-      'User-Agent': 'request',
-      'Accept': '*/*',
-      'Cache-Control': 'no-cache',
-      'Host': 'watcha.com',
-      'accept-encoding': 'gzip, deflate',
-      'Connection': 'keep-alive'
-    }
-  };
-  request(options, (err, results, body) => {
-    try {
-      var result = {
-        result: {}
-      };
-      let titleList = {};
-      console.log(results);
-      res.end(body);
-      return;
-      body = iconv.decode(body, 'euc-kr');
-      console.log(body);
-      const $ = cheerio.load(body);
-      const $bodyList = $.find("li.css-106b4k6-Self");
-
-      $bodyList.each(function(i, elem) {
-        result.result[$(this).find('a').attr('href').replace('/ko-KR/contents/', '')] = $(this).find('.css-gt67eo-TopResultItemTitle').text();
-      });
-      res.end(result);
-    } catch (e) {
-      console.log(e);
-      res.sendStatus(500);
-    } finally {
-
+      'Host': 'watcha.com'
     }
   });
+
+  instance.get('/search', {
+    params: {
+      'query': req.params.movie
+    }
+  }).then(response => {
+    console.log(response);
+    res.sendStatus(200);
+  });
+
+
+  return;
+
+  const options = {
+    uri: "https://watcha.com/ko-KR/search?query=" + req.params.movie,
+    headers: {
+      '': ''
+    }
+  };
+  request(options, (err, response, body) => {
+    console.log(body);
+    res.end(body);
+  });
+  var result = {
+    result: {}
+  };
+  let titleList = {};
+  return;
+  console.log(body);
+  const $ = cheerio.load(body);
+  const $bodyList = $.find("li.css-106b4k6-Self");
+
+  $bodyList.each(function(i, elem) {
+    result.result[$(this).find('a').attr('href').replace('/ko-KR/contents/', '')] = $(this).find('.css-gt67eo-TopResultItemTitle').text();
+  });
+  res.end(result);
 });
 
 router.get('/search/watcha/:movie', (req, res, next) => {
