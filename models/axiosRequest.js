2b91818c075814da56252b0cefe3fe9c6b0ae489
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-07-01 23:53:07 +0900

    something

--- models/axiosRequest.js
+++ models/axiosRequest.js
@@ -1,7 +1,6 @@
 /*jshint esversion: 9 */
 
 const axios = require("axios");
-const cheerio = require("cheerio");
 
 const createInstance = (url) => {
   axios.create({
---
+++ modules/movieSync.js
@@ -0,0 +1,10 @@
+/*jshint esversion: 9 */
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const movie = watchaID => {
+
+};
+
+module.exports = movie;
