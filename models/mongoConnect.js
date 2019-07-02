0dc423cba4e170958497303fb55cc0ec1ce44622
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-07-02 16:02:50 +0900

    .

--- models/mongoConnect.js
+++ models/mongoConnect.js
@@ -1,2 +1,17 @@
+/*jshint esversion: 9 */
+
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+
+
+const url = 'mongodb://127.0.0.1:27017';
+// Database Name
+var dbo;
+const dbName = 'hive';
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("연결 성공");
+  dbo = client.db(dbName);
+});
+
+module.exports = dbo;
