11f4e6852ae517403bb34f57d670cf25f8a5f8db
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-07-02 16:22:54 +0900

    DB 로그인 추가

--- models/mongoConnect.js
+++ models/mongoConnect.js
@@ -2,15 +2,14 @@
 
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const mongoUser = require('../modules/userGetter');
 
-
-const url = 'mongodb://127.0.0.1:27017';
+const url = `mongodb://${mongoUser.id}:${mongoUser.pw}@54.180.27.126:27017`;
 // Database Name
 var dbo;
 const dbName = 'hive';
 MongoClient.connect(url, function(err, client) {
   assert.equal(null, err);
-  console.log("연결 성공");
   dbo = client.db(dbName);
 });
 
--- modules/movieSync.js
+++ modules/movieSync.js
@@ -2,6 +2,7 @@
 
 const axios = require('axios');
 const cheerio = require('cheerio');
+const db = require('../models/mongoConnect');
 
 const movie = watchaID => {
 
