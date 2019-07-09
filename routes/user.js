bf6d119b4754a62fff0a9f46772d572c564b3067
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-07-09 20:49:35 +0900

    SID!

---
+++ routes/user.js
@@ -0,0 +1,12 @@
+/*jshint esversion: 9 */
+
+const express = require('express');
+const sid = require('@startergate/sidjs');
+const router = express.Router();
+
+/* GET home page. */
+router.post('/login', (req, res, next) => {
+  sid.
+});
+
+module.exports = router;
