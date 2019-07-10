7d3ed9e75297c41da51dfeb0e6713e4c663ec18a
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-07-10 14:24:08 +0900

    다시 세팅

---
+++ public/javascripts/sid.js
@@ -0,0 +1,132 @@
+/*jshint esversion: 9 */
+
+class SID {
+  constructor(clientname) {
+    if (typeof jQuery == 'undefined') {
+      throw new Error("SID.js for Client requires jQuery");
+    }
+    if (!window.localStorage) {
+      throw new Error("SID.js for Client requires Local Storage support in browser");
+    }
+    if (navigator.userAgent.indexOf('MSIE') !== -1 || navigator.userAgent.indexOf('Trident') !== -1) { //Client Browser Detection
+      throw new Error("SID.js for Client don't support IE");
+    }
+    this.clientname = clientname;
+  }
+
+  checkID(id, callback) {
+    $.ajax({
+      url: 'http://sid.donote.co:3000/api/v1/id/' + id + '/exist/bool',
+      type: 'GET',
+      dataType: 'json',
+      success: function(data) {
+        callback(data.is_exist);
+      }
+    });
+  }
+
+  getProfile(clientid, sessid, callback) {
+    $.ajax({
+      url: 'http://sid.donote.co:3000/api/v1/pfimg',
+      type: 'GET',
+      dataType: 'json',
+      data: {
+        'type': 'get',
+        'data': 'pfimg',
+        'clientid': clientid,
+        'sessid': sessid
+      },
+      success: (data) => {
+        callback(data.requested_data);
+      }
+    });
+  }
+
+  getClientID() {
+    if (localStorage.sid_clientid) {
+      return localStorage.sid_clientid;
+    }
+    return false;
+  }
+
+  getSessID() {
+    if (localStorage.sid_sessid) {
+      return localStorage.sid_sessid;
+    }
+    return false;
+  }
+
+  createClientID(devicedata) {
+    $.ajax({
+      url: 'http://sid.donote.co:3000/api/v1/clientid',
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        'type': 'create',
+        'data': 'clientid',
+        'devicedata': devicedata
+      },
+      success: (data) => {
+        localStorage.sid_clientid = data.response_data;
+      }
+    });
+  }
+
+  login(clientid, id, pw, callback) {
+    console.log({
+      'type': 'login',
+      'clientid': clientid,
+      'userid': id,
+      'password': pw,
+      'isPermanent': false,
+      'isWeb': true,
+      'isClient': true
+    });
+    $.ajax({
+      url: 'http://sid.donote.co:3000/api/v1/session/',
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        'type': 'login',
+        'clientid': clientid,
+        'userid': id,
+        'password': pw,
+        'isPermanent': false,
+        'isWeb': true,
+        'isClient': true
+      },
+      success: (data) => {
+        var output = {};
+        if (data.type == 'error') {
+          callback({
+            error: 1
+          });
+          return
+        }
+        output.sessid = data.response_data[0];
+        output.pid = data.response_data[1];
+        output.nickname = data.response_data[2];
+        output.expire = data.response_data[3];
+        callback(output);
+      }
+    }).fail();
+  }
+
+  register(clientid, id, pw, nickname, callback) {
+    $.ajax({
+      url: 'http://sid.donote.co:3000/api/v1/user/',
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        'type': 'register',
+        'clientid': clientid,
+        'userid': id,
+        'nickname': nickname,
+        'password': pw
+      },
+      success: (data) => {
+        callback(data.private_id);
+      }
+    });
+  }
+}
--- routes/user.js
+++ routes/user.js
@@ -5,8 +5,23 @@ const sid = require('@startergate/sidjs');
 const router = express.Router();
 
 /* GET home page. */
-router.post('/login', (req, res, next) => {
-  sid.
+router.all('/', (req, res, next) => {
+  res.redirect('./liked');
+});
+
+router.get('/liked', (req, res, next) => {
+  res.render('liked');
+});
+
+router.get('/login', (req, res, next) => {
+  sid.loginAuth(req.param.clientid, req.param.sessid).then(result => {
+    req.session.sessid = result.sessid;
+    req.session.pid = result.pid;
+    req.session.nickname = result.nickname;
+    req.session.expire = result.expire;
+    res.redirect('/');
+  })
+  res.redirect('/');
 });
 
 module.exports = router;
